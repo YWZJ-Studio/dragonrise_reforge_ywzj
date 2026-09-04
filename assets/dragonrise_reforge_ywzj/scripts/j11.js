da61c746e12b1ac9557d0b6dd82ee684ffd6cd54
Ranpoes <2213321305@qq.com>
2026-09-04 16:38:35 +0800

    完善歼-11B

--- assets/dragonrise_reforge_ywzj/scripts/j11.js
+++ assets/dragonrise_reforge_ywzj/scripts/j11.js
@@ -4,11 +4,12 @@ function updateBones(context) {
     const rollInput = context.getRollInput()
 
     const builder = createPoseBuilder()
-    builder.setRotation("LeftTailPlane2", -pitchInput * 16, 0, 0)
-    builder.setRotation("RightTailPlane2", -pitchInput * 16, 0, 0)
-    builder.setRotation("wingLB", rollInput * 16, 0, 0)
-    builder.setRotation("wingRB", -rollInput * 16, 0, 0)
+    builder.setRotation("wingLB", -pitchInput * 16, 0, 0)
+    builder.setRotation("wingRB", -pitchInput * 16, 0, 0)
+    builder.setRotation("wingLR", rollInput * 16, 0, 0)
+    builder.setRotation("wingRR", -rollInput * 16, 0, 0)
     builder.setRotation("LeftVerticalTail", 0, -yawInput * 14, 0)
     builder.setRotation("RightVerticalTail", 0, -yawInput * 14, 0)
+    builder.setRotation("ctrl", -8 * pitchInput, 0, -8 * rollInput);
     return builder
 }
