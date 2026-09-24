db54d377e5c79609ed0e77296b64a3a2bc30b83d
Ranpoes <2213321305@qq.com>
2026-09-25 00:16:52 +0800

    完善J-35

--- assets/dragonrise_reforge_ywzj/scripts/j35.js
+++ assets/dragonrise_reforge_ywzj/scripts/j35.js
@@ -4,9 +4,12 @@ function updateBones(context) {
     const rollInput = context.getRollInput()
 
     const builder = createPoseBuilder()
-    builder.setRotation("wingLB", -pitchInput * 16 + rollInput * 16, 0, 0)
-    builder.setRotation("wingRB", -pitchInput * 16 - rollInput * 16, 0, 0)
+    builder.setRotation("wingLR", rollInput * 16, 0, 0)
+    builder.setRotation("wingRR", -rollInput * 16, 0, 0)
+    builder.setRotation("wingLB", pitchInput * 16, 0, 0)
+    builder.setRotation("wingRB", pitchInput * 16, 0, 0)
     builder.setRotation("bone", 0, -yawInput * 14, 0)
     builder.setRotation("bone2", 0, -yawInput * 14, 0)
+    builder.setRotation("ctrl", -8 * pitchInput, 0, -8 * rollInput)
     return builder
 }
