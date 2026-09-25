d9e7e1df7c59417010680a980c5f9d12e0230abd
Ranpoes <2213321305@qq.com>
2026-09-25 20:55:20 +0800

    完善Su-24M

--- assets/dragonrise_reforge_ywzj/scripts/su24m.js
+++ assets/dragonrise_reforge_ywzj/scripts/su24m.js
@@ -1,10 +1,28 @@
+const missiles_kh_29 = ["kh29", "kh29-1"]
+const missiles_r_60 = ["missle2", "missle3"]
+
 function updateBones(context) {
     const pitchInput = context.getPitchInput()
     const yawInput = context.getYawInput()
     const rollInput = context.getRollInput()
 
     const builder = createPoseBuilder()
-    builder.setRotation("13", -pitchInput * 14 + rollInput * 12, 0, 0)
-    builder.setRotation("2", -pitchInput * 14 - rollInput * 12, 0, 0)
+    builder.setRotation("elevator_left", pitchInput * 14, 0, 0)
+    builder.setRotation("elevator_right", pitchInput * 14, 0, 0)
+    builder.setRotation("rudder", 0, -yawInput * 14, 0)
+    builder.setRotation("ctrl", -8 * pitchInput, 0, -8 * rollInput)
+    builder.setRotation("ctrl2", -8 * pitchInput, 0, -8 * rollInput)
+    const remainKh29 = context.getWeaponRemainAmmo("sighting_system", 1)
+    for (let i = 0; i < missiles_kh_29.length; i++) {
+        if (i < missiles_kh_29.length - remainKh29) {
+            builder.hideBone(missiles_kh_29[i])
+        }
+    }
+    const remainR60 = context.getWeaponRemainAmmo("sighting_system", 2)
+    for (let i = 0; i < missiles_r_60.length; i++) {
+        if (i < missiles_r_60.length - remainR60) {
+            builder.hideBone(missiles_r_60[i])
+        }
+    }
     return builder
 }
