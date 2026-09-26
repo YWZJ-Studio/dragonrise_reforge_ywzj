86c88fbbf5bac68cefd54fae3daf129082dbb400
Ranpoes <2213321305@qq.com>
2026-09-26 14:57:43 +0800

    完善F-16C

--- assets/dragonrise_reforge_ywzj/scripts/f16c.js
+++ assets/dragonrise_reforge_ywzj/scripts/f16c.js
@@ -4,10 +4,11 @@ function updateBones(context) {
     const rollInput = context.getRollInput()
 
     const builder = createPoseBuilder()
-    builder.setRotation("tailwingL", -pitchInput * 16, 0, 0)
-    builder.setRotation("tailwingR", -pitchInput * 16, 0, 0)
-    builder.setRotation("flapL", rollInput * 16, 0, 0)
-    builder.setRotation("flapR", -rollInput * 16, 0, 0)
-    builder.setRotation("bone4", 0, -yawInput * 14, 0)
+    builder.setRotation("elevator_left", pitchInput * 16, 0, 0)
+    builder.setRotation("elevator_right", pitchInput * 16, 0, 0)
+    builder.setRotation("aileron_left", rollInput * 16, 0, 0)
+    builder.setRotation("aileron_right", -rollInput * 16, 0, 0)
+    builder.setRotation("rudder", 0, -yawInput * 14, 0)
+    builder.setRotation("ctrl", -8 * pitchInput, 0, -8 * rollInput)
     return builder
 }
