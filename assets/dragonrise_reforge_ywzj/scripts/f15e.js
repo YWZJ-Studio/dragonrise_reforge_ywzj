0f042786ea94bf66c1dbbb005e38f94fb122808f
Ranpoes <2213321305@qq.com>
2026-09-26 16:50:20 +0800

    完善F-15E

--- assets/dragonrise_reforge_ywzj/scripts/f15e.js
+++ assets/dragonrise_reforge_ywzj/scripts/f15e.js
@@ -4,9 +4,12 @@ function updateBones(context) {
     const rollInput = context.getRollInput()
 
     const builder = createPoseBuilder()
-    builder.setRotation("flapL", -pitchInput * 14 + rollInput * 14, 0, 0)
-    builder.setRotation("flapR", -pitchInput * 14 - rollInput * 14, 0, 0)
-    builder.setRotation("VFL", 0, -yawInput * 12, 0)
-    builder.setRotation("VFR", 0, -yawInput * 12, 0)
+    builder.setRotation("group22", 0, 0, rollInput * 14)
+    builder.setRotation("group2", 0, 0, -rollInput * 14)
+    builder.setRotation("flapL", -pitchInput * 14, 0, 0)
+    builder.setRotation("flapR", -pitchInput * 14, 0, 0)
+    builder.setRotation("group20", 0, -yawInput * 12, 0)
+    builder.setRotation("group19", 0, -yawInput * 12, 0)
+    builder.setRotation("ctrl", -8 * pitchInput, 0, -8 * rollInput)
     return builder
 }
